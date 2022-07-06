dcf54fc2431fa7b91d79ea71aa6ddb443adc2244
Keks <keks@htmlacademy.ru>
2022-07-06 20:38:31 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,19 @@
+const generateRandom = function(min, max) {
+  if (min < 0 && max >= min) {
+    return 0;
+  }
+  if (min > max) {
+    return NaN;
+  }
+  if (min === max) {
+    return min;
+  }
+  return Math.floor(Math.random() * (max - (min - 1))) + min;
+};
+generateRandom(6, 66);
+
+const checkCommentLength = function (comment, permissibleLength) {
+  return (comment.length <= permissibleLength);
+};
+checkCommentLength('Hello World!', 5);
+
